6d4b67b48788ee5e1596bd33b8f68da6c24d8892
juanantonioruz <juanantonioruz@gmail.com>
2014-01-08 20:25:26 +0100

    doc improvements

--- src/ew_snmp/SNMPApi.clj
+++ src/ew_snmp/SNMPApi.clj
@@ -7,17 +7,17 @@
 
 
 (defn -v2
-   "host comm eso y eso
-  y otra cosa ,as "
   [h c oid]
-
   (first (getv2 h c oid))
   )
-(comment
-  (-v2 "localhost" "public" "1.3.6.1.2.1.1.5.0"))
 
 
 (defn -v3 [ip u p oid]
   (first (getv3 ip u p oid)))
+
+(comment
+  (-v2 "localost" "public" "1.3.6.1.2.1.1.5.0"))
+
+
 (comment
   (-v3 "127.0.0.1" "juanv3" "comomolalagramola" "1.3.6.1.2.1.1.5.0"))
--- src/ew_snmp/v2.clj
+++ src/ew_snmp/v2.clj
@@ -1,3 +1,6 @@
+;; ## this ns try to encapsulate all the knowledge releated with NSMP v2c
+;; so far only we worried about "get" actions
+;; maybe differents types of community-target, pdus and GET options (GET, GETNEXT, GETBULK)
 (ns ew_snmp.v2
   (:import
     [org.snmp4j Snmp PDU CommunityTarget]
@@ -9,8 +12,10 @@
     )
   )
 
-(defn build-target [host community]
+(defn build-target
   "Returns CommunityTarget object"
+  [host community]
+
   (doto (CommunityTarget.)
     (.setCommunity (new OctetString community))
     (.setAddress (GenericAddress/parse (format "udp:%s/161" host)))
@@ -20,48 +25,68 @@
     )
   )
 
-;(println (str "@" (build-target "host" "public")))
+(comment
+  (println (str "@" (build-target "host" "public"))))
 
-(defn build-pdu [oid]
+(defn build-pdu
   "Build PDU Object"
+  [oid]
   (doto (PDU.)
     (.setType PDU/GET)
     (.addAll (into-array (map #(VariableBinding. (OID. (str %))) oid)))
     )
   )
 
-;(build-pdu '("1.3.6.1.2.1.1.3.0" "1.3.6.1.2.1.2.2.1.21.1"))
+(comment
+  (build-pdu '("1.3.6.1.2.1.1.3.0" "1.3.6.1.2.1.2.2.1.21.1")))
 
 
-
+;; checking response and by the way printing the response type on console
 (defn response? [response]
   (let [t (. response getType)]
     (println t)
    (and response (== (. response getType) PDU/RESPONSE)))
   )
-;(response? nil)
-
-
-
-(defn getv2 [host community & oid]
-  (do
-    (def pdu (build-pdu oid))
-    (def target (build-target host community))
-    (def snmp (Snmp. (DefaultUdpTransportMapping.)))
-    (. snmp listen)
-    (def event (. snmp send pdu target nil))
-    (def response (. event getResponse))
-    (. snmp close)
-    (when (response? response)
-      ; return data.
-      (vec (map #(do
-               (let [xv (. % getVariable)]
-                 #_(println % xv)
-                 (str xv))) (. response getVariableBindings)))
-    )
-  )
-  )
 (comment
+  (response? nil))
+
+;; **core function**
+;;
+;;    this function retuns an array [] of return values or exceptions cached
+;;
+;;    `(= ["Right here, right now."] (getv2 "localhost" "public" "1.3.6.1.2.1.1.6.0"))`
+;;
+;;    `(= ["java.lang.NullPointerException"] (getv2 "error" "public" "1"))`
+;;
+;;    `(= ["java.lang.NullPointerException"] (getv2 "localhost" "error" "1"))`
+;;
+(defn getv2 [host community & oid]
+  (try
+
+    (do
+      (def pdu (build-pdu oid))
+      (def target (build-target host community))
+      (def snmp (Snmp. (DefaultUdpTransportMapping.)))
+      (. snmp listen)
+
+      (def event (. snmp send pdu target nil))
+      (def response (. event getResponse))
+      (. snmp close)
+      (when (response? response)
+                                        ; return data.
+        (vec (map #(do
+                     (let [xv (. % getVariable)]
+                       #_(println % xv)
+                       (str xv))) (. response getVariableBindings)))
+        )
+      )
+    (catch NullPointerException e [(str  e)]))
+  )
+
+(comment
+  (= ["Right here, right now."] (getv2 "localhost" "public" "1.3.6.1.2.1.1.6.0"))
+  (= ["java.lang.NullPointerException"] (getv2 "error" "public" "1"))
+  (= ["java.lang.NullPointerException"] (getv2 "localhost" "error" "1"))
   (vec
    (map
     #(getv2 "localhost" "public" % ) ["1.3.6.1.2.1.1.5.0" "1.3.6.1.2.1.1.6.0" ".1.3.6.1.2.1.25.3.6.1.4" "1.3.6.1.2.1.31.1.1.1.1.0" "1.3.6.1.2.1.1.3.0"])))
